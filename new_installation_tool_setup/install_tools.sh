daaeb569fdb995e72f54a07ec0d447f2044aecb2
GavrilS <gavr1lll@abv.bg>
2025-05-03 18:22:43 +0300

    Added install commands for bittorrent and also performance command to check availability of free disk space

---
+++ new_installation_tool_setup/install_tools.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install bittorrent
+sudo apt update
+sudo apt upgrade
+sudo apt install qbittorrent
+# to run bittorrent through the terminal use:
+qbittorrent
---
+++ performance_commands/general.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Disk space can be monitored with the 'df'(disk-free) command
+df
+df -h
