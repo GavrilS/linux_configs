84056fc6be3de91958bdce15c6ebfb94c9a49d9a
GavrilS <gavr1lll@abv.bg>
2025-08-06 23:04:01 +0300

    Added a script to install Docker on ubuntu using the apt repository

---
+++ docker/install_docker_ubuntu.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Uninstall old versions
+echo "*** Uninstall old versions"
+echo "Uninstall old versions of the docker related packages"
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc;
+do 
+    sudo apt-get remove $pkg;
+done
+
+# Install docker using the apt repository
+echo "*** Install docker using the apt repository"
+# Add Docker's official GPG key
+echo "*** Add Docker's official GPG key"
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to apt sources
+echo "*** Add the repository to apt sources"
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install the Docker packages
+echo "*** Install the Docker packages"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verify the installation is successful by running the 'hello-world' image
+echo "*** Verify the installation is successful by running the 'hello-world' image"
+sudo docker run hello-world
